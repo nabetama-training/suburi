e8eb7be3b844ca83892c82331070340dbf2604df
nabetama <mao.nabeta@gmail.com>
2022-10-07 23:11:02 +0900

    fixed file

--- src/Easy/0001-two-sum.test.ts
+++ src/Easy/0001-two-sum.test.ts
@@ -1,3 +1,9 @@
+describe('0001-two-sum', () => {
+  test('Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.', () => {
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+  });
+});
+
 const twoSum = (nums: number[], target: number): number[] => {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length; j++) {
@@ -11,5 +17,3 @@ const twoSum = (nums: number[], target: number): number[] => {
   }
   return [0];
 };
-
-export {twoSum};
--- src/__test__/0001-two-sum.test.ts
+++
@@ -1,7 +0,0 @@
-import {twoSum} from '../0001-two-sum';
-
-describe('0001-two-sum', () => {
-  test('Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.', () => {
-    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
-  });
-});
