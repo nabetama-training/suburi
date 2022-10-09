ff1f68df91dded1d251d3fa9780dfc4fab0a28ae
nabetama <mao.nabeta@gmail.com>
2022-10-09 09:26:30 +0900

    14. Longest Common Prefix

--- src/Easy/0014-longest-common-prefix.test.ts
+++ src/Easy/0014-longest-common-prefix.test.ts
@@ -1,7 +1,3 @@
-interface Counter {
-  [key: string]: number;
-}
-
 function longestCommonPrefix(strs: string[]): string {
   if (!strs.length) {
     return '';
@@ -10,16 +6,41 @@ function longestCommonPrefix(strs: string[]): string {
     return strs[0];
   }
 
+  strs.sort();
+  let result = '';
+
+  // 配列はソート済みなので、先頭要素の文字列と末尾要素の文字列で共通する接頭辞は、
+  // 配列のすべての要素で共通であることが保証されている
+  for (let i = 0; i < strs[0].length; i++) {
+    if (strs[0][i] === strs[strs.length - 1][i]) {
+      result = result.concat(strs[0][i]);
+    } else {
+      break;
+    }
+  }
+
   // no common
-  return '';
+  return result;
 }
 
-describe.only('longestCommonPrefix', () => {
+describe('longestCommonPrefix', () => {
   test('empty array', () => {
     expect(longestCommonPrefix([])).toBe('');
   });
 
   test('array contains only one element', () => {
     expect(longestCommonPrefix(['foo'])).toBe('foo');
+  });
+
+  test('array contains multiple elements there have common prefix', () => {
+    expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toBe('fl');
+  });
+
+  test('array contains multiple elements, which have a common prefix', () => {
+    expect(longestCommonPrefix(['flower', 'flow', 'flight'])).toBe('fl');
+  });
+
+  test('array contains multiple elements but no common prefix', () => {
+    expect(longestCommonPrefix(['dog', 'racecar', 'car'])).toBe('');
   });
 });
