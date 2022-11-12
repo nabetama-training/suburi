7d6b1ef3ade8291a1133f2f4bbc0865e80f8c943
nabetama <mao.nabeta@gmail.com>
2022-11-12 09:35:04 +0900

    prettier with coc test

--- src/Easy/0035-Search-Insert-Position.test.ts
+++ src/Easy/0035-Search-Insert-Position.test.ts
@@ -5,33 +5,33 @@
  * @return target が見つかればそのインデックス。見つからなければ挿入されたときのインデックスを返す
  */
 const searchInsert = (nums: number[], target: number): number => {
-  let start = 0;
-  let end = nums.length - 1;
+	let start = 0;
+	let end = nums.length - 1;
 
-  while (start <= end) {
-    const middle = start + Math.trunc((end - start) / 2);
-    if (nums[middle] === target) {
-      return middle;
-    } else if (nums[middle] < target) {
-      start = middle + 1;
-    } else {
-      end = middle - 1;
-    }
-  }
+	while (start <= end) {
+		const middle = start + Math.trunc((end - start) / 2);
+		if (nums[middle] === target) {
+			return middle;
+		} else if (nums[middle] < target) {
+			start = middle + 1;
+		} else {
+			end = middle - 1;
+		}
+	}
 
-  return start;
+	return start;
 };
 
 describe('Search Insert Position', () => {
-  test('ex1', () => {
-    expect(searchInsert([1, 3, 5, 6], 5)).toBe(2);
-  });
+	test('ex1', () => {
+		expect(searchInsert([1, 3, 5, 6], 5)).toBe(2);
+	});
 
-  test('ex2', () => {
-    expect(searchInsert([1, 3, 5, 6], 2)).toBe(1);
-  });
+	test('ex2', () => {
+		expect(searchInsert([1, 3, 5, 6], 2)).toBe(1);
+	});
 
-  test('ex3', () => {
-    expect(searchInsert([1, 3, 5, 6], 7)).toBe(4);
-  });
+	test('ex3', () => {
+		expect(searchInsert([1, 3, 5, 6], 7)).toBe(4);
+	});
 });
