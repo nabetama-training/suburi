686b70d34d40022540f394e71a12c2d9aa215e37
nabetama <mao.nabeta@gmail.com>
2023-03-08 21:49:02 +0900

    add solve

--- src/easy/remove_duplicates_from_sorted_array.rs
+++ src/easy/remove_duplicates_from_sorted_array.rs
@@ -1,5 +1,6 @@
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
     // https://doc.rust-lang.org/std/vec/struct.Vec.html#method.dedup
+    // using only sorted array
     nums.dedup();
     nums.len() as i32
 }
@@ -19,6 +20,23 @@ pub fn remove_duplicates2(nums: &mut Vec<i32>) -> i32 {
 
     // 全体から重複した回数をマイナス → ユニークになった数字の個数
     (nums.len() - duplicate_count) as i32
+}
+
+pub fn remove_duplicates3(nums: &mut Vec<i32>) -> i32 {
+    let mut prev_number: Option<i32> = None; // 前回の数値
+    let mut i = 0;
+
+    // nums.len() は動的に変化する
+    while i < nums.len() {
+        if prev_number == Some(nums[i]) {
+            nums.remove(i); // 前回の数値と同じ場合、今回の要素を削除する
+        } else {
+            prev_number = Some(nums[i]); // 前回の数値と異なっていれば、配列操作は行わず、前回の数値（prev_number）を更新する
+            i += 1;
+        }
+    }
+
+    nums.len() as i32
 }
 
 #[cfg(test)]
@@ -73,6 +91,29 @@ mod test {
         for mut tc in test_cases {
             // assert_eq!(tc.output, remove_duplicates(&mut tc.input));
             assert_eq!(tc.output, remove_duplicates2(&mut tc.input));
+        }
+    }
+
+    #[test]
+    fn test_remove_duplicates3() {
+        let test_cases = [
+            TestCase {
+                input: vec![1, 1, 2],
+                output: 2,
+            },
+            TestCase {
+                input: vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4],
+                output: 5,
+            },
+            TestCase {
+                input: vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
+                output: 6,
+            },
+        ];
+
+        for mut tc in test_cases {
+            // assert_eq!(tc.output, remove_duplicates(&mut tc.input));
+            assert_eq!(tc.output, remove_duplicates3(&mut tc.input));
         }
     }
 }
