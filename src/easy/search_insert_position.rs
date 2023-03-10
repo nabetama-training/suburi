4b2ac43a7b126d75cf634f498c8cd7c7d1470d8a
nabetama <mao.nabeta@gmail.com>
2023-03-10 17:57:30 +0900

    35. Search Insert Position https://leetcode.com/problems/search-insert-position/description/

--- src/easy/search_insert_position.rs
+++ src/easy/search_insert_position.rs
@@ -1,5 +1,40 @@
+use std::cmp::Ordering;
+
+// Given a sorted array of distinct integers and a target value,
+// return the index if the target is found.
+// If not, return the index where it would be if it were inserted in order.
+//
+// You must write an algorithm with O(log n) runtime complexity.
+pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+    if nums.is_empty() {
+        return 0;
+    }
+
+    if target < *nums.first().unwrap() {
+        return 0;
+    }
+
+    // binary serach
+    let mut low = 0;
+    let mut high = nums.len() - 1;
+
+    while low <= high {
+        let mid = (low + high) / 2;
+
+        match target.cmp(&nums[mid]) {
+            Ordering::Equal => return mid as i32,
+            Ordering::Greater => low = mid + 1,
+            Ordering::Less => high = mid - 1,
+        }
+    }
+
+    low as i32
+}
+
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
     fn test_search_insert() {
         struct TestCase {
