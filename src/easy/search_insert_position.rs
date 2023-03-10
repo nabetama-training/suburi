4ba037f2bef3eccc0bb6dba3390cfe0118a2c9ac
nabetama <mao.nabeta@gmail.com>
2023-03-10 18:17:02 +0900

    add test

--- src/easy/search_insert_position.rs
+++ src/easy/search_insert_position.rs
@@ -80,4 +80,35 @@ mod test {
             assert_eq!(tc.output, search_insert(tc.nums, tc.target));
         }
     }
+
+    #[test]
+    fn test_search_insert2() {
+        struct TestCase {
+            nums: Vec<i32>,
+            target: i32,
+            output: i32,
+        }
+
+        let test_cases = [
+            TestCase {
+                nums: vec![1, 3, 5, 6],
+                target: 5,
+                output: 2,
+            },
+            TestCase {
+                nums: vec![1, 3, 5, 6],
+                target: 2,
+                output: 1,
+            },
+            TestCase {
+                nums: vec![1, 3, 5, 6],
+                target: 7,
+                output: 4,
+            },
+        ];
+
+        for tc in test_cases {
+            assert_eq!(tc.output, search_insert2(tc.nums, tc.target));
+        }
+    }
 }
