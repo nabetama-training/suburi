9981e5ae85d3c044e5a9d93e70e9438e5b35bac9
nabetama <mao.nabeta@gmail.com>
2023-04-13 22:24:40 +0900

    .

--- src/bin/abs/abc081_a.rs
+++ src/bin/abs/abc081_a.rs
@@ -11,13 +11,29 @@ fn main() {
 mod test {
     #[test]
     fn test_main() {
-        let input = "101";
-        let output = "2\n";
-        assert_cmd::Command::cargo_bin("abc081_a")
-            .unwrap()
-            .write_stdin(input)
-            .assert()
-            .success()
-            .stdout(output);
+        struct TestCase {
+            input: String,
+            output: String,
+        }
+
+        let test_cases = [
+            TestCase {
+                input: "101".to_string(),
+                output: "2\n".to_string(),
+            },
+            TestCase {
+                input: "000".to_string(),
+                output: "0\n".to_string(),
+            },
+        ];
+
+        for tc in test_cases {
+            assert_cmd::Command::cargo_bin("abc081_a")
+                .unwrap()
+                .write_stdin(tc.input)
+                .assert()
+                .success()
+                .stdout(tc.output);
+        }
     }
 }
