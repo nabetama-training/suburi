8ec72953fa2aad6fbf16f94ad615d3be752b52e8
nabetama <mao.nabeta@gmail.com>
2023-04-19 23:21:09 +0900

    abc083_b

--- src/bin/abs/abc083_b.rs
+++ src/bin/abs/abc083_b.rs
@@ -13,11 +13,13 @@ fn main() {
         let mut tmp = i;
         let mut sum_tmp = 0;
 
+        // 1桁ずつ足す
         while tmp > 0 {
             sum_tmp += tmp % 10;
             tmp /= 10;
         }
 
+        // a以上b以下の sum を求める
         if sum_tmp >= a && sum_tmp <= b {
             sum += i;
         }
@@ -33,13 +35,29 @@ mod test {
             output: &'static str,
         }
 
-        let test_cases = [TestCase {
-            input: r#"20
+        let test_cases = [
+            TestCase {
+                input: r#"20
 2
 5"#,
-            output: r#"84
+                output: r#"84
 "#,
-        }];
+            },
+            TestCase {
+                input: r#"10
+1
+2"#,
+                output: r#"13
+"#,
+            },
+            TestCase {
+                input: r#"100
+4
+16"#,
+                output: r#"4554
+"#,
+            },
+        ];
 
         for tc in test_cases.iter() {
             assert_cmd::Command::cargo_bin("abc083_b")
